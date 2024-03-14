627c9c9b252dffffeafc0a331c628c89aba895df
aaa785246 <aaa785246@gmail.com>
2024-03-14 10:26:16 +0800

    113/3/14 檔案匯入
    作品集

---
+++ sp-junyou/src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+//import './App.css';
+import * as React from 'react'
+import colors from './theme/colors';
+import './theme/homePage.css';
+import {AlertDialog,AlertDialogBody,AlertDialogFooter,AlertDialogHeader,
+  AlertDialogContent,AlertDialogOverlay,AlertDialogCloseButton} from '@chakra-ui/react'
+
+function App() {
+  
+  return (
+    
+      <div  className='header'>
+      <div id='main1'>
+        面面相覷
+      </div>
+      <div id='main2'>
+        測試
+      </div>
+      <div id='main3'>
+        登入
+      </div>
+    </div>
+    
+    
+  )
+
+}
+
+
+export default App;
