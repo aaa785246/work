cabd2a000cc4a536bc47ad7dacf21503e6e29786
aaa785246 <aaa785246@gmail.com>
2024-03-14 16:34:59 +0800

    3/14
    圖片更新

--- sp-junyou/src/App.js
+++ sp-junyou/src/App.js
@@ -1,31 +1,95 @@
 // import logo from './logo.svg';
 //import './App.css';
-import * as React from 'react'
-import colors from './theme/colors';
-import './theme/homePage.css';
-import {AlertDialog,AlertDialogBody,AlertDialogFooter,AlertDialogHeader,
-  AlertDialogContent,AlertDialogOverlay,AlertDialogCloseButton} from '@chakra-ui/react'
+import * as React from "react";
+import colors from "./theme/colors";
+import "./theme/homePage.css";
+// import {
+//   AlertDialog,
+//   AlertDialogBody,
+//   AlertDialogFooter,
+//   AlertDialogHeader,
+//   AlertDialogContent,
+//   AlertDialogOverlay,
+//   AlertDialogCloseButton,
+//   useDisclosure,
+//   Button,
+//   ChakraProvider,
+//   styled,
+//   Box
+// } from "@chakra-ui/react";
 
 function App() {
-  
   return (
-    
-      <div  className='header'>
-      <div id='main1'>
-        面面相覷
+    <div>
+      <div className="header">
+        <div id="header1">面面相覷</div>
+        <div id="header2">主功能介紹</div>
+        <div id="header3">登入</div>
       </div>
-      <div id='main2'>
-        測試
-      </div>
-      <div id='main3'>
-        登入
+      <div className="bgsup">
+        <div className="imageContainer">
+          <img className="homeImg" src="./theme/img/suit.jpg" alt="suit"></img>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
-    
-    
-  )
-
+  );
 }
 
+// function LoginAlert({ state }) {
+//   const { isOpen, onOpen, onClose } = useDisclosure();
+//   const cancelRef = React.useRef();
+//   var dialogWork = "";
+//   var dialogColorSelect = "";
+//   var dialogHover = "";
+
+//   if (state == 1) {
+//     dialogWork = "登入";
+//     dialogColorSelect = "colors.gray.sup";
+//     dialogHover = "colors.gray.900";
+//     return (
+//       <>
+//         <Button
+//           bg={dialogColorSelect}
+//           onClick={onOpen}
+//           _hover={{ bg: dialogHover }}
+//         >
+//           {dialogWork}
+//         </Button>
+
+//         <AlertDialog
+//           isOpen={isOpen}
+//           leastDestructiveRef={cancelRef}
+//           onClose={onClose}
+//         >
+//           <AlertDialogOverlay>
+//             <AlertDialogContent>
+//               <AlertDialogHeader fontSize="20px" fontWeight="bold">
+//                 確定要{dialogWork}嗎?
+//               </AlertDialogHeader>
+
+//               <AlertDialogBody>{dialogWork}後將無法復原</AlertDialogBody>
+
+//               <AlertDialogFooter>
+//                 <Button ref={cancelRef} onClick={onClose}>
+//                   取消
+//                 </Button>
+//                 <Button
+//                   bg={dialogColorSelect}
+//                   _hover={{ bg: dialogHover }}
+//                   onClick={onClose}
+//                   ml={3}
+//                 >
+//                   {dialogWork}
+//                 </Button>
+//               </AlertDialogFooter>
+//             </AlertDialogContent>
+//           </AlertDialogOverlay>
+//         </AlertDialog>
+//       </>
+//     );
+//   }
+// }
 
 export default App;
