cbb2570e6abf78d0422fe40193b5e49f22c70d04
aaa785246 <aaa785246@gmail.com>
2024-03-28 11:46:22 +0800

    0328 1146
    爽刻一波

--- vue-junyou/src/views/article.vue
+++ vue-junyou/src/views/article.vue
@@ -2,18 +2,36 @@
 import "@/assets/article.css";
 import { ref, watch } from "vue";
 
-var title = "測試文章";
-var user = "羅志祥";
-var content =
-  "至於為什麼要思考面試呢？其實是有更深層的原因，面試的發生，到底需要如何做到，不面試的發生，又會如何產生。你真的了解面試嗎？奧斯特洛夫斯基曾經說過，共同的事業，共同的鬥爭，可以使人們產生忍受一切的力量。　這似乎非常的有道理，對吧？我們一般認為，抓住了問題的關鍵，其他一切則會迎刃而解。就我個人來說，面試對我的意義，不能不說非常重大。";
-var heartCount = 84;
-var bookImgSrc="@/img/heart.png";
+
+//標題、發文者、內容
+const title = ref("前端工程師面試阿阿阿阿阿阿阿");
+const user = ref("羅志祥");
+const content = ref(
+  "至於為什麼要思考面試呢？其實是有更深層的原因，面試的發生，到底需要如何做到，不面試的發生，又會如何產生。你真的了解面試嗎？奧斯特洛夫斯基曾經說過，共同的事業，共同的鬥爭，可以使人們產生忍受一切的力量。　這似乎非常的有道理，對吧？我們一般認為，抓住了問題的關鍵，其他一切則會迎刃而解。就我個人來說，面試對我的意義，不能不說非常重大。"
+);
+//留言者、留言內容
+const messageName = ref("小火龍");
+const msg = ref(
+  "最近去了很多家軟工面試，這家是我目前面試下來最有趣的，一開始一樣是正常的自我介紹與一些面試官的問題，在自我介紹與一些簡單的問題後"
+);
+//愛心數
+const heartCount = ref(84);
+// 開啟書籤換圖片
+const bookMarkImgSrc = ref(`${import.meta.env.BASE_URL}src/img/bookmark.png`);
+// 開啟書籤調整class
+const bookMarkClass = ref("ar-bookmark");
+//開啟書籤調整分享按鈕
+const share = ref("ar-share");
+//開啟關閉開關
 const myMarkToggle = ref(false);
 const toggleMark = () => {
-    myMarkToggle.value !=  myMarkToggle.value
-    bookImgSrc = myMarkToggle ? "@/img/bookmarkOn.png" : "@/img/bookmark.png";
-}
-
+  myMarkToggle.value = !myMarkToggle.value;
+  bookMarkImgSrc.value = myMarkToggle.value
+    ? `${import.meta.env.BASE_URL}src/img/bookmarkOn.png`
+    : `${import.meta.env.BASE_URL}src/img/bookmark.png`;
+  bookMarkClass.value = myMarkToggle.value ? "ar-bookmarkOn" : "ar-bookmark";
+  share.value = myMarkToggle.value ? "ar-shareOn" : "ar-share";
+};
 </script>
 
 <template>
@@ -28,10 +46,31 @@ const toggleMark = () => {
   <div class="article-title">{{ title }}</div>
   <div class="article-user">{{ user }}</div>
   <div class="article-content">{{ content }}</div>
+
   <div class="ar-heartAndMsg-container">
     <img src="@/img/heart.png" alt="heart" class="ar-heart" />
     <div class="ar-heartCount">{{ heartCount }}</div>
-    <img src="@/img/bookmark.png" alt="bookmark" class="ar-bookmark" :class= "myMarkToggle? 'bookMarkOn':'bookMarkOff'"  @click="toggleMark"/>
-    <img src="@/img/share.png" alt="share" class="ar-share" />
+    <img
+      :src="bookMarkImgSrc"
+      alt="bookmark"
+      :class="bookMarkClass"
+      @click="toggleMark"
+    />
+    <img src="@/img/share.png" alt="share" :class="share" />
+  </div>
+  <div class="ar-allMsg">所有留言</div>
+  <!-- 到時候用for迴圈跑 第一個框的 class="ar-articleBox"與第二個框之後class="ar-articleBox2"
+  margin不同需要額外做處理-->
+  <div class="ar-articleBox">
+    <div class="ar-messageName">{{ messageName }}</div>
+    <div class="ar-content">{{ msg }}</div>
+  </div>
+  <div class="ar-articleBox2">
+    <div class="ar-messageName">{{ messageName }}</div>
+    <div class="ar-content">{{ msg }}</div>
+  </div>
+  <div class="ar-articleBox2">
+    <div class="ar-messageName">{{ messageName }}</div>
+    <div class="ar-content">{{ msg }}</div>
   </div>
 </template>
--- vue-junyou/src/views/shareExp.vue
+++ vue-junyou/src/views/shareExp.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
 import "@/assets/shareExp.css";
-var artic = "前端工程師面試阿阿阿阿阿阿阿";
-var msg =
-  "最近去了很多家軟工面試，這家是我目前面試下來最有趣的，一開始一樣是正常的自我介紹與一些面試官的問題，在自我介紹與一些簡單的問題後";
-if (msg.length > 30) {
-  msg = msg.substring(0, 60) + "....";
+import { ref } from 'vue';
+
+//標題、內容
+const artic = ref("前端工程師面試阿阿阿阿阿阿阿");
+const msg = ref("最近去了很多家軟工面試，這家是我目前面試下來最有趣的，一開始一樣是正常的自我介紹與一些面試官的問題，在自我介紹與一些簡單的問題後");
+//標題或內容太長做裁減
+if (msg.value.length > 30) {
+  msg.value = msg.value.substring(0, 30) + "....";
 }
-if (artic.length > 7) {
-  artic = artic.substring(0, 7) + "....";
+if (artic.value.length > 7) {
+  artic.value = artic.value.substring(0, 7)+ "..";
 }
-var heartCount = "80";
+//愛心數
+var heartCount = ref(80) ;
+//留言數
+const msgCount = ref(8);
 </script>
 
 <template>
@@ -33,42 +39,44 @@ var heartCount = "80";
       class="sh-searchBoxGlass"
     />
   </div>
-  <RouterLink to="/">
+  <!-- 到時候用for迴圈跑 *要注意第一個框是 class="articleBox"與第二個框class="articleBox2" 
+    margin設定不同 要額外處理-->
+  <RouterLink to="/article">
     <div class="articleBox">
     <div class="sh-title">{{ artic }}</div>
     <div class="sh-content">{{ msg }}</div>
     <div class="heartAndMsg-container">
       <img src="@/img/heart.png" alt="heart" class="heart" />
-      <div class="heartCount">{{ heartCount }}</div>
-      <img src="@/img/message2.png" alt="heart" class="message" />
-      <div class="heartCount">{{ heartCount }}</div>
+      <div class="heartAndMsgCount">{{ heartCount }}</div>
+      <img src="@/img/message2.png" alt="message" class="message" />
+      <div class="heartAndMsgCount">{{ msgCount }}</div>
     </div>
   </div>
   </RouterLink>
  
-  <RouterLink to="/">
+  <RouterLink to="/article">
     <div class="articleBox2">
     <div class="sh-title">{{ artic }}</div>
     <div class="sh-content">{{ msg }}</div>
     <div class="heartAndMsg-container">
       <img src="@/img/heart.png" alt="heart" class="heart" />
-      <div class="heartCount">{{ heartCount }}</div>
+      <div class="heartAndMsgCount">{{ heartCount }}</div>
       <img src="@/img/message2.png" alt="heart" class="message" />
-      <div class="heartCount">{{ heartCount }}</div>
+      <div class="heartAndMsgCount">{{ msgCount }}</div>
     </div>
   </div>
   </RouterLink>
   
 
-  <RouterLink to="/">
+  <RouterLink to="/article">
     <div class="articleBox2">
     <div class="sh-title">{{ artic }}</div>
     <div class="sh-content">{{ msg }}</div>
     <div class="heartAndMsg-container">
       <img src="@/img/heart.png" alt="heart" class="heart" />
-      <div class="heartCount">{{ heartCount }}</div>
+      <div class="heartAndMsgCount">{{ heartCount }}</div>
       <img src="@/img/message2.png" alt="heart" class="message" />
-      <div class="heartCount">{{ heartCount }}</div>
+      <div class="heartAndMsgCount">{{ msgCount }}</div>
     </div>
   </div>
   </RouterLink>
